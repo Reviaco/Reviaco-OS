6f6654435b84fc8d7c43fb1854116c8eceb99978
Ahmed Hassan <reviaco@outlook.com>
2016-06-13 17:21:31 +0300

    Update index.php

--- System/UI/Settings/index.php
+++ System/UI/Settings/index.php
@@ -252,11 +252,11 @@ $ds = disk_total_space("C:");
 <h4></h4><paper-icon-item onclick='' id="deletition"><div class="avatar blue" item-icon></div><paper-item-body two-line><div>Delete Account</div><div id="description" secondary></div></paper-item-body><paper-icon-button icon="star" alt="favourite this!"></paper-icon-button></paper-icon-item>
                 
             </div>
-                        <canvas id="app-chart" width="100" height="100" />
+                        <canvas id="app-chart chart" width="100" height="100" />
         </section>
         <section id="Storage_settings" class="Storage_settings">
 
-            <canvas id="storage-chart" width="100" height="100" />
+            <canvas id="storage-chart chart" width="100" height="100" />
         </section>
 
 
