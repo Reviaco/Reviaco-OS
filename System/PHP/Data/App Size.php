a3e1ee296bbe14dbdef43106ce561638a0aead19
Ahmed Hassan <reviaco@outlook.com>
2016-06-13 15:38:37 +0300

    Update App Size.php

--- System/PHP/Data/App Size.php
+++ System/PHP/Data/App Size.php
@@ -2,7 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 $stringData = $_POST['dataString'];
-$filename = '../../../'$stringData'';
+$filename = '../../../"$stringData"';
 
 echo  filesize($filename);
 ?>
