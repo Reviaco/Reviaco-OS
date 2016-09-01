ea330c348ad0362b0561f86d0082a1983533d8d2
Ahmed0Hassan <reviaco@outlook.com>
2016-09-01 16:55:08 +0200

    Installer UI

--- System/UI/Installer/index.php
+++ System/UI/Installer/index.php
@@ -7,13 +7,35 @@
 </head>
 <body>
 
-  <div class="sw">
-    <form>
-      <input type="search" class="search" placeholder="Search Echo Store..." />
+<paper-toolbar>
+  <paper-icon-button icon="menu"></paper-icon-button>
+  <span class="title">Echo Store</span>
+  <paper-icon-button icon="search"></paper-icon-button>
+</paper-toolbar>
+<div class="app_page">
+<div id="appIntroduction_bg" class="appIntroduction_bg">
 
-      <a href="#" class="logo" title="GMRUI"></a>
-    </form>
-  </div>
+<video id="appIntroduction" class="appIntroduction" src="src" type="video/mp4" autoplay loop></video>
+</div>
+<paper-fab id="install_btn" class="install_btn" icon="file-download" title="Install" class="orange"></paper-fab>
+</div>   
+</div>      
+    
+
+
+<div class="space3d">
+        
+    
+    <div class="_3dbox">
+      <div class="_3dface _3dface--front"></div>
+      <div class="_3dface _3dface--top"></div>
+      <div class="_3dface _3dface--bottom"></div>
+      <div class="_3dface _3dface--left"></div>
+      <div class="_3dface _3dface--right"></div>
+      <div class="_3dface _3dface--back"></div>
+    </div>
+    
+</div>
 
 
 </body>
