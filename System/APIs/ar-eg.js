22898d83ebbcb46ef540c4763484869fadca1f75
Ahmed0Hassan <reviaco@outlook.com>
2017-02-17 22:08:09 +0200

    Updating Polymer

--- System/APIs/ar-eg.js
+++ System/APIs/ar-eg.js
@@ -245,46 +245,46 @@ if (settings.الشفافية) {
         return this.each(function() {
             var name = settings.الاسم;
             var icon = settings.الايكونة;
-            var out = '<paper-icon id="' + name + '" icon="' + icon + '"></paper-icon>';
+            var out = '<iron-icon id="' + name + '" icon="' + icon + '"></iron-icon>';
             $('Eطبق_السلطة').append(out);
 
             if (settings.لون_الايكونة) {
-                if (settings.لون_الخط == 'اسود') {
+                if (settings.لون_الايكونة == 'اسود') {
                     $('#' + name + '').css('color', '#000000');
-                } else if (settings.لون_الخط == 'احمر') {
+                } else if (settings.لون_الايكونة == 'احمر') {
                     $('#' + name + '').css('color', '#F44336');
-                } else if (settings.لون_الخط == 'ازرق') {
+                } else if (settings.لون_الايكونة == 'ازرق') {
                     $('#' + name + '').css('color', '#2196F3');
-                } else if (settings.لون_الخط == 'اورانج') {
+                } else if (settings.لون_الايكونة == 'اورانج') {
                     $('#' + name + '').css('color', '#FF9800');
-                } else if (settings.لون_الخط == 'لبنى') {
+                } else if (settings.لون_الايكونة == 'لبنى') {
                     $('#' + name + '').css('color', '#00BCD4');
-                } else if (settings.لون_الخط == 'رصاصى') {
+                } else if (settings.لون_الايكونة == 'رصاصى') {
                     $('#' + name + '').css('color', '#9E9E9E');
-                } else if (settings.لون_الخط == 'اخضر') {
+                } else if (settings.لون_الايكونة == 'اخضر') {
                     $('#' + name + '').css('color', '#4CAF50');
-                } else if (settings.لون_الخط == 'ابيض') {
+                } else if (settings.لون_الايكونة == 'ابيض') {
                     $('#' + name + '').css('color', '#FFFFFF');
-                } else if (settings.لون_الخط == 'برتقانى') {
+                } else if (settings.لون_الايكونة == 'برتقانى') {
                     $('#' + name + '').css('color', '#FF9800');
-                } else if (settings.لون_الخط == 'برتقالى') {
+                } else if (settings.لون_الايكونة == 'برتقالى') {
                     $('#' + name + '').css('color', '#FF9800');
-                } else if (settings.لون_الخط == 'اصفر') {
+                } else if (settings.لون_الايكونة == 'اصفر') {
                     $('#' + name + '').css('color', '#FFEB3B');
-                } else if (settings.لون_الخط == 'بنى') {
+                } else if (settings.لون_الايكونة == 'بنى') {
                     $('#' + name + '').css('color', '#795548');
-                } else if (settings.لون_الخط == 'تركواز') {
+                } else if (settings.لون_الايكونة == 'تركواز') {
                     $('#' + name + '').css('color', '#009688');
-                } else if (settings.لون_الخط == 'بينك') {
+                } else if (settings.لون_الايكونة == 'بينك') {
                     $('#' + name + '').css('color', '#E91E63');
-                } else if (settings.لون_الخط == 'بمبى') {
+                } else if (settings.لون_الايكونة == 'بمبى') {
                     $('#' + name + '').css('color', '#E91E63');
-                } else if (settings.لون_الخط == 'روز') {
+                } else if (settings.لون_الايكونة == 'روز') {
                     $('#' + name + '').css('color', '#E91E63');
-                } else if (settings.لون_الخط == 'اخضر_فاتح') {
+                } else if (settings.لون_الايكونة == 'اخضر_فاتح') {
                     $('#' + name + '').css('color', '#8BC34A');
                 } else {
-                $('#' + name + '').css('color', settings.لون_الخط);
+                $('#' + name + '').css('color', settings.لون_الايكونة);
 }
             }
 
@@ -316,19 +316,14 @@ if (settings.الشفافية) {
     };
     $.fn.زرار_بأيكونة = function(options) {
 
-        // Establish our default settings
+                // Establish our default settings
         var settings = $.extend({
-            الكلام: 'انت شكلك نسيت تكتب حاجة',
-            لون_الخط: null,
+            لون_الايكونة: null,
+            الايكونة: null,
             الحجم: null,
             الاسم: null,
             العرض: null,
             الطول: null,
-            الطخن: null,
-            شكل_الخط: null,
-            مقفول: null,
-            مرفوع: null,
-            سويتش: null,
             الحركة: null,
             لما_يخلص_تحميل: null
         }, options);
@@ -339,71 +334,47 @@ if (settings.الشفافية) {
             var out = '<paper-icon-button id="' + name + '" icon="' + icon + '"></paper-icon-button>';
             $('Eطبق_السلطة').append(out);
 
-            if (settings.لون_الخط) {
-                if (settings.لون_الخط == 'اسود') {
+            if (settings.لون_الايكونة) {
+                if (settings.لون_الايكونة == 'اسود') {
                     $('#' + name + '').css('color', '#000000');
-                } else if (settings.لون_الخط == 'احمر') {
+                } else if (settings.لون_الايكونة == 'احمر') {
                     $('#' + name + '').css('color', '#F44336');
-                } else if (settings.لون_الخط == 'ازرق') {
+                } else if (settings.لون_الايكونة == 'ازرق') {
                     $('#' + name + '').css('color', '#2196F3');
-                } else if (settings.لون_الخط == 'اورانج') {
+                } else if (settings.لون_الايكونة == 'اورانج') {
                     $('#' + name + '').css('color', '#FF9800');
-                } else if (settings.لون_الخط == 'لبنى') {
+                } else if (settings.لون_الايكونة == 'لبنى') {
                     $('#' + name + '').css('color', '#00BCD4');
-                } else if (settings.لون_الخط == 'رصاصى') {
+                } else if (settings.لون_الايكونة == 'رصاصى') {
                     $('#' + name + '').css('color', '#9E9E9E');
-                } else if (settings.لون_الخط == 'اخضر') {
+                } else if (settings.لون_الايكونة == 'اخضر') {
                     $('#' + name + '').css('color', '#4CAF50');
-                } else if (settings.لون_الخط == 'ابيض') {
+                } else if (settings.لون_الايكونة == 'ابيض') {
                     $('#' + name + '').css('color', '#FFFFFF');
-                } else if (settings.لون_الخط == 'برتقانى') {
+                } else if (settings.لون_الايكونة == 'برتقانى') {
                     $('#' + name + '').css('color', '#FF9800');
-                } else if (settings.لون_الخط == 'برتقالى') {
+                } else if (settings.لون_الايكونة == 'برتقالى') {
                     $('#' + name + '').css('color', '#FF9800');
-                } else if (settings.لون_الخط == 'اصفر') {
+                } else if (settings.لون_الايكونة == 'اصفر') {
                     $('#' + name + '').css('color', '#FFEB3B');
-                } else if (settings.لون_الخط == 'بنى') {
+                } else if (settings.لون_الايكونة == 'بنى') {
                     $('#' + name + '').css('color', '#795548');
-                } else if (settings.لون_الخط == 'تركواز') {
+                } else if (settings.لون_الايكونة == 'تركواز') {
                     $('#' + name + '').css('color', '#009688');
-                } else if (settings.لون_الخط == 'بينك') {
+                } else if (settings.لون_الايكونة == 'بينك') {
                     $('#' + name + '').css('color', '#E91E63');
-                } else if (settings.لون_الخط == 'بمبى') {
+                } else if (settings.لون_الايكونة == 'بمبى') {
                     $('#' + name + '').css('color', '#E91E63');
-                } else if (settings.لون_الخط == 'روز') {
+                } else if (settings.لون_الايكونة == 'روز') {
                     $('#' + name + '').css('color', '#E91E63');
-                } else if (settings.لون_الخط == 'اخضر_فاتح') {
+                } else if (settings.لون_الايكونة == 'اخضر_فاتح') {
                     $('#' + name + '').css('color', '#8BC34A');
                 } else {
-                $('#' + name + '').css('color', settings.لون_الخط);
+                $('#' + name + '').css('color', settings.لون_الايكونة);
 }
             }
 
-                        if (settings.شكل_الخط) {
-
-                $('#' + name + '').css('font-style', settings.شكل_الخط);
-            }
-            
- if (settings.مقفول == 'ايوة') {
-     $('#' + name + '').attr('disabled','');
-            }
-            
-             if (settings.مرفوع == 'ايوة') {
-     $('#' + name + '').attr('raised','');
-            }
-            
-                         if (settings.سويتش == 'ايوة') {
-     $('#' + name + '').attr('toggles','');
-            }
-            
-            if (settings.الطخن) {
-                if (settings.الطخن == 'طخين') {
-$('#' + name + '').css('font-weight', 'bold');
-                   
-                } else {
- $('#' + name + '').css('font-weight', settings.الطخن);
-}
-            }
+                        
             if (settings.الحجم) {
                 $('#' + name + '').css('font-size', settings.الحجم);
             }
@@ -427,9 +398,8 @@ if (settings.الشفافية) {
                 settings.لما_يخلص_تحميل.call(this);
             }
         });
-
     };
-    $.fn.كونتينر = function(options) {
+    $.fn.سيكشن = function(options) {
 
         // Establish our default settings
         var settings = $.extend({
@@ -450,78 +420,57 @@ if (settings.الشفافية) {
 
         return this.each(function() {
             var name = settings.الاسم;
-            var out = '<paper-button id="' + name + '"></paper-button>';
+            var out = '<paper-card id="' + name + '"><div id="الحاجات_اللى_جوة_السيكشن" class="card-content"></div></paper-card>';
             $('Eطبق_السلطة').append(out);
-            $('#' + name + '').text(settings.الكلام);
 
-            if (settings.لون_الخط) {
-                if (settings.لون_الخط == 'اسود') {
-                    $('#' + name + '').css('color', '#000000');
-                } else if (settings.لون_الخط == 'احمر') {
-                    $('#' + name + '').css('color', '#F44336');
-                } else if (settings.لون_الخط == 'ازرق') {
-                    $('#' + name + '').css('color', '#2196F3');
-                } else if (settings.لون_الخط == 'اورانج') {
-                    $('#' + name + '').css('color', '#FF9800');
-                } else if (settings.لون_الخط == 'لبنى') {
-                    $('#' + name + '').css('color', '#00BCD4');
-                } else if (settings.لون_الخط == 'رصاصى') {
-                    $('#' + name + '').css('color', '#9E9E9E');
-                } else if (settings.لون_الخط == 'اخضر') {
-                    $('#' + name + '').css('color', '#4CAF50');
-                } else if (settings.لون_الخط == 'ابيض') {
-                    $('#' + name + '').css('color', '#FFFFFF');
-                } else if (settings.لون_الخط == 'برتقانى') {
-                    $('#' + name + '').css('color', '#FF9800');
-                } else if (settings.لون_الخط == 'برتقالى') {
-                    $('#' + name + '').css('color', '#FF9800');
-                } else if (settings.لون_الخط == 'اصفر') {
-                    $('#' + name + '').css('color', '#FFEB3B');
-                } else if (settings.لون_الخط == 'بنى') {
-                    $('#' + name + '').css('color', '#795548');
-                } else if (settings.لون_الخط == 'تركواز') {
-                    $('#' + name + '').css('color', '#009688');
-                } else if (settings.لون_الخط == 'بينك') {
-                    $('#' + name + '').css('color', '#E91E63');
-                } else if (settings.لون_الخط == 'بمبى') {
-                    $('#' + name + '').css('color', '#E91E63');
-                } else if (settings.لون_الخط == 'روز') {
-                    $('#' + name + '').css('color', '#E91E63');
-                } else if (settings.لون_الخط == 'اخضر_فاتح') {
-                    $('#' + name + '').css('color', '#8BC34A');
+
+            if (settings.الخلفية) {
+                if (settings.الخلفية == 'سودة') {
+                    $('#' + name + '').css('background', '#000000');
+                } else if (settings.الخلفية == 'حمرة') {
+                    $('#' + name + '').css('background', '#F44336');
+                } else if (settings.الخلفية == 'زرقة') {
+                    $('#' + name + '').css('background', '#2196F3');
+                } else if (settings.الخلفية == 'اورانج') {
+                    $('#' + name + '').css('background', '#FF9800');
+                } else if (settings.الخلفية == 'لبنى') {
+                    $('#' + name + '').css('background', '#00BCD4');
+                } else if (settings.الخلفية == 'رصاصى') {
+                    $('#' + name + '').css('background', '#9E9E9E');
+                } else if (settings.الخلفية == 'خضرة') {
+                    $('#' + name + '').css('background', '#4CAF50');
+                } else if (settings.الخلفية == 'بيضة') {
+                    $('#' + name + '').css('background', '#FFFFFF');
+                } else if (settings.الخلفية == 'برتقانى') {
+                    $('#' + name + '').css('background', '#FF9800');
+                } else if (settings.الخلفية == 'برتقالى') {
+                    $('#' + name + '').css('background', '#FF9800');
+                } else if (settings.الخلفية == 'صفرة') {
+                    $('#' + name + '').css('background', '#FFEB3B');
+                } else if (settings.الخلفية == 'بنى') {
+                    $('#' + name + '').css('background', '#795548');
+                } else if (settings.الخلفية == 'تركواز') {
+                    $('#' + name + '').css('background', '#009688');
+                } else if (settings.الخلفية == 'بينك') {
+                    $('#' + name + '').css('background', '#E91E63');
+                } else if (settings.الخلفية == 'بمبى') {
+                    $('#' + name + '').css('background', '#E91E63');
+                } else if (settings.الخلفية == 'روز') {
+                    $('#' + name + '').css('background', '#E91E63');
+                } else if (settings.الخلفية == 'اخضر_فاتح') {
+                    $('#' + name + '').css('background', '#8BC34A');
                 } else {
-                $('#' + name + '').css('color', settings.لون_الخط);
+                $('#' + name + '').css('background', settings.الخلفية);
 }
             }
 
-                        if (settings.شكل_الخط) {
 
-                $('#' + name + '').css('font-style', settings.شكل_الخط);
+            
+             if (settings.مصدر_الصورة) {
+                $('#' + name + '').attr('image', settings.مصدر_الصورة);
             }
             
- if (settings.مقفول == 'ايوة') {
-     $('#' + name + '').attr('disabled','');
-            }
-            
-             if (settings.مرفوع == 'ايوة') {
-     $('#' + name + '').attr('raised','');
-            }
-            
-                         if (settings.سويتش == 'ايوة') {
-     $('#' + name + '').attr('toggles','');
-            }
-            
-            if (settings.الطخن) {
-                if (settings.الطخن == 'طخين') {
-$('#' + name + '').css('font-weight', 'bold');
-                   
-                } else {
- $('#' + name + '').css('font-weight', settings.الطخن);
-}
-            }
-            if (settings.الحجم) {
-                $('#' + name + '').css('font-size', settings.الحجم);
-            }
+
             if (settings.العرض) {
                 $('#' + name + '').css('width', settings.العرض);
             }
